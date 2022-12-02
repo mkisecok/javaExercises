64300db754f2c084388ce56610b2b1d77c4dd41e
Mahmut <mahmut.kisecok@mineral-minds.de>
2022-12-02 16:23:11 +0100

    Add singleTon service file

--- src/exercises/singletonpattern/SingletonService.java
+++ src/exercises/singletonpattern/SingletonService.java
@@ -1,19 +1,29 @@
-package exercises.SingletonPattern;
+package exercises.singletonpattern;
+
+import kisecok.testproject.Color;
 
 public class SingletonService {
 
+
     public static void main(String[] args) {
+
+
 
         DoubleCheckedSingleton doubleCheckedSingleton_1 = DoubleCheckedSingleton.getInstance();
         DoubleCheckedSingleton doubleCheckedSingleton_2 = DoubleCheckedSingleton.getInstance();
+       // DoubleCheckedSingleton doubleCheckedSingleton_3 = new DoubleCheckedSingleton();
 
-        System.out.println(doubleCheckedSingleton_1.equals(doubleCheckedSingleton_2));
+        //System.out.println(DoubleCheckedSingleton.StringConstant);
+
+       // System.out.println(doubleCheckedSingleton_1.equals(doubleCheckedSingleton_2));
 
 
         BillPughSingleton billPughSingleton_1 = BillPughSingleton.getInstance();
         BillPughSingleton billPughSingleton_2 = BillPughSingleton.getInstance();
 
-        System.out.println(billPughSingleton_1.equals(billPughSingleton_2));
+        //System.out.println(billPughSingleton_1.equals(billPughSingleton_2));
+
+
 
     }
 }
